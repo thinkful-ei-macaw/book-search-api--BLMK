4b35b0e7a363dd3629c2c0f5d65c7f1da6c32466
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-09 16:02:11 -0400

    Updates

---
+++ precache-manifest.2c40b5c35781d621885f29faa370b85b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b40f7874d48e7e2e8d3a51ac38fc3a",
+    "url": "/book-search-api--BLMK/index.html"
+  },
+  {
+    "revision": "af0e0485f07c69182abe",
+    "url": "/book-search-api--BLMK/static/css/main.90ab2ae7.chunk.css"
+  },
+  {
+    "revision": "7346ae30803ef77f3971",
+    "url": "/book-search-api--BLMK/static/js/2.f40a5239.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/book-search-api--BLMK/static/js/2.f40a5239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af0e0485f07c69182abe",
+    "url": "/book-search-api--BLMK/static/js/main.7051fc4c.chunk.js"
+  },
+  {
+    "revision": "f0c77880b28987407658",
+    "url": "/book-search-api--BLMK/static/js/runtime-main.d96e002b.js"
+  }
+]);
